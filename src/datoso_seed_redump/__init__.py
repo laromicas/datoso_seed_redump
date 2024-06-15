28f41e8ab6275e4178da37f41670e35a1131455b
laromicas <laromicas@hotmail.com>
2024-06-14 22:37:20 -0500

    Update datoso_seed_redump version

--- src/datoso_seed_redump/__init__.py
+++ src/datoso_seed_redump/__init__.py
@@ -1,6 +1,6 @@
 """Initialize seed variables."""
 __all__ = ['__version__', '__author__', '__description__']
-__version__ = '1.0b0.dev0'
+__version__ = '1.0.0rc1'
 __author__ = 'Lacides Miranda'
 __description__ = ('Redump.org is a disc preservation database and internet community dedicated to collecting precise '
     'and accurate information about every video game ever released on optical media of any system.')
