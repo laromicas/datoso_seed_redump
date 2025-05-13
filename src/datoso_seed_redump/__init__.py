8379c7dc33c891673e72f388cb280a4b8c4b3669
laromicas <laromicas@hotmail.com>
2025-05-13 00:06:39 -0500

    - Bump version - Ruff code changes

--- src/datoso_seed_redump/__init__.py
+++ src/datoso_seed_redump/__init__.py
@@ -1,6 +1,6 @@
 """Initialize seed variables."""
-__all__ = ['__version__', '__author__', '__description__']
-__version__ = '1.0.0'
+__all__ = ['__author__', '__description__', '__version__']
+__version__ = '1.0.1'
 __author__ = 'Lacides Miranda'
 __description__ = ('Redump.org is a disc preservation database and internet community dedicated to collecting precise '
     'and accurate information about every video game ever released on optical media of any system.')
